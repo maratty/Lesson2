bd152cd0b7def76fb18b7a6a179ab08d6c68e493
John Smith <marraty@mail.ru>
2025-02-25 20:11:36 +0300

    a

--- src/main/java/com/zad/Tovar.java
+++ src/main/java/com/zad/Tovar.java
@@ -19,10 +19,10 @@ public class Tovar {
 
          System.out.println("Название:" + tovar1.name);
          System.out.println("Дата производства:" + tovar1.date);
-        System.out.println("Производитель:" + tovar1.made);
-        System.out.println("Страна происхождения:" + tovar1.country);
-        System.out.println("Цена:" + tovar1.price);
-        System.out.println("Состояние бронирования покупателемм:" + tovar1.booking);
+         System.out.println("Производитель:" + tovar1.made);
+         System.out.println("Страна происхождения:" + tovar1.country);
+         System.out.println("Цена:" + tovar1.price);
+         System.out.println("Состояние бронирования покупателемм:" + tovar1.booking);
 
     }
 }
