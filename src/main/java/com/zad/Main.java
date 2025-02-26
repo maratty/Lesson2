634e7832ca759477d5ca0f62438c53c68b6451e2
John Smith <marraty@mail.ru>
2025-02-26 23:33:41 +0300

    26

---
+++ src/main/java/com/zad/Main.java
@@ -0,0 +1,24 @@
+package com.zad;
+
+class Park {
+    String name = "Карусель";
+    String time = "10.00 - 22.00";
+    String price = "100 рублей";
+
+    String age = "с 5 до 10 лет";
+
+    class Inner {
+        String nameIn = "наименование парка:";
+        String timeIn = "время работы:";
+        String priceIn = "стоимость:";
+    }
+}
+public class Main{
+
+    public static void main(String[] args) {
+        Park myOuter = new Park();
+        Park.Inner myInner = myOuter.new Inner();
+        System.out.println(myInner.timeIn + myOuter.time);
+    }
+}
+
